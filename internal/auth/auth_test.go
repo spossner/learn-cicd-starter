fb66a3061edea56d9a440a430125090aca8c85c1
spossner <github@possner.de>
2024-11-15 21:23:43 +0100

    add tests step into ci/cd pipeline - with test broken

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,34 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	type args struct {
+		headers http.Header
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{"simple", args{headers: http.Header{"Authorization": {"ApiKey 1234"}}}, "1234", false},
+		{"missing", args{headers: http.Header{}}, "", false},
+		{"wrong", args{headers: http.Header{"Authorization": {"Bearer 1234"}}}, "", true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetAPIKey(tt.args.headers)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetAPIKey() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GetAPIKey() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
